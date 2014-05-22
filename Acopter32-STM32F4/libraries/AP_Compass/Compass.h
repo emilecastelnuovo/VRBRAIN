ffedb75af7381083a7b396ca865d86e91ca12372
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-05-22 18:26:16 +0200

    Update on libraries.

--- Acopter32-STM32F4/libraries/AP_Compass/Compass.h
+++ Acopter32-STM32F4/libraries/AP_Compass/Compass.h
@@ -14,6 +14,7 @@
 #define AP_COMPASS_TYPE_HMC5843  0x02
 #define AP_COMPASS_TYPE_HMC5883L 0x03
 #define AP_COMPASS_TYPE_PX4      0x04
+#define AP_COMPASS_TYPE_VRBRAIN  0x05
 
 // motor compensation types (for use with motor_comp_enabled)
 #define AP_COMPASS_MOT_COMP_DISABLED    0x00
